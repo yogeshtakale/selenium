72060eae5ea136b4fc8db622acd5b6d2dff3713b
yYakale <ytakale446@gmail.com>
2024-03-06 13:49:31 +0530

    added

--- Baseclass.java
+++ Baseclass.java
@@ -1,11 +1,14 @@
 package CommonUtils;
 
+import javax.sql.rowset.WebRowSet;
+
 import org.testng.annotations.BeforeSuite;
 
 public class Baseclass {
 	@BeforeSuite
 	public void bs() {
-		
+		WebRowSet.class
+	
 	}
 	//@b
 	
